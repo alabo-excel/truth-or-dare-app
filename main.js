6d4cf38a06ae43a5939dc524e03dff05f7c61013
alabo-excel <iamalaboexcel@gmail.com>
2020-07-18 17:27:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+var truth = document.getElementById('truth')
+var dare = document.getElementById('dare')
+
+
+var truths = new Array(
+    "Have you ever kissed an animal?",
+    "Have you ever cheated on a test?",
+    "What was the last thing you ate?",
+    "Do you have any unusual talents?",
+    "Do you have any phobias?",
+    "Have you ever used someone else's password?",
+    "Have you ever ridden the bus without paying the fare?",
+    "Do you message people during your classes?",
+    "Have you ever fallen asleep during a class?",
+    "Have you ever bitten a toenail?",
+    "Have you ever stolen something?",
+    "Are you a hard-working student?",
+    "What was the best day of you life?",
+    "What was the strangest dream you ever had?",
+    "What is the most annoying thing to you (pet peeve)?",
+    "If you could have a superpower, what would it be?",
+    "Who is most important to you?"
+);
+
+var dares = new Array(
+    "Sing a song.",
+    "Post 'I love English!' on social media.",
+    "Say the English alphabet backwards.",
+    "Give someone near you a compliment.",
+    "Show the last photo you took with your phone.",
+    "Do your best dance.",
+    "Act like a cat.",
+    "Act like a monkey and a donkey at the same time.",
+    "Introduce yourself to someone you don't know.",
+    "Smell the inside of your shoe.",
+    "Call someone and ask if they believe in aliens.",
+    "Act like a pirate.",
+    "Talk about the last time you apologized.",
+    "Act like you are swimming.",
+    "Say the months of the year backwards."
+);
+
+
+dare.addEventListener('click', () =>{
+    var num = Math.floor(Math.random()*14);
+    document.getElementById('span').innerHTML = dares[num];
+    console.log(dares[num])
+})
+
+truth.addEventListener('click', () =>{
+    var num = Math.floor(Math.random()*14);
+    document.getElementById('span').innerHTML = truths[num];
+    console.log(truths[num])
+})
+
